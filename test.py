9ec4259b65c6c51b2dffdac054de40dc6cb1bab7
Itsbailey X <baileyleah542@gmail.com>
2023-03-26 02:38:38 +0100

    fixed gen bug

--- test.py
+++ test.py
@@ -10,7 +10,7 @@ for x in range(0,100, 1):
     elif randomBuilding==2:
         Entity(model='quad',texture='assets/textures/building2', x=x)
 
-for xM in range(0,-100,-1):
+for xM in range(-1,-100,-1):
     randomBuilding=ra.randint(1,2)
     if randomBuilding==1:
         Entity(model='quad',texture='assets/textures/building1', x=xM)
