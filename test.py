f855acd77144317dcac7363d415d5c49a5c23b15
Itsbailey X <baileyleah542@gmail.com>
2023-03-26 02:32:06 +0100

    Silly

--- test.py
+++ test.py
@@ -3,7 +3,11 @@ import random as ra
 
 app=Ursina()
 
-bg1=Entity(model='quad',texture='assets/textures/building1')
-bg2=Entity(model='quad',texture='assets/textures/building2')
+for x in range(0,100, 20):
+    randomBuilding=ra.randint(1,2)
+    if randomBuilding==1:
+        bg1=Entity(model='quad',texture='assets/textures/building1', x=x)
+    elif randomBuilding==2:
+        bg2=Entity(model='quad',texture='assets/textures/building2', x=x)
 
 app.run()
