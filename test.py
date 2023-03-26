800d624b3cec54ebea913db7c4d1ffa79c33e1b1
Itsbailey X <baileyleah542@gmail.com>
2023-03-26 02:23:34 +0100

    eggs

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import random as ra
 
 app=Ursina()
 
-bg1=Entity(model='assets/textures/building1.png')
-bg2=Entity(model='assets/textures/building2.png')
+bg1=Entity(model='quad',texture='assets/textures/building1')
+bg2=Entity(model='quad',texture='assets/textures/building2')
 
-app=Ursina()
+app.run()
