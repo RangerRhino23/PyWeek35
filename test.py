fb20d3721709da028641ae6dc57b6fd0a08addd9
Itsbailey X <baileyleah542@gmail.com>
2023-03-26 02:36:47 +0100

    random generation

--- test.py
+++ test.py
@@ -3,11 +3,18 @@ import random as ra
 
 app=Ursina()
 
-for x in range(0,100, 20):
+for x in range(0,100, 1):
     randomBuilding=ra.randint(1,2)
     if randomBuilding==1:
-        bg1=Entity(model='quad',texture='assets/textures/building1', x=x)
+        Entity(model='quad',texture='assets/textures/building1', x=x)
     elif randomBuilding==2:
-        bg2=Entity(model='quad',texture='assets/textures/building2', x=x)
+        Entity(model='quad',texture='assets/textures/building2', x=x)
+
+for xM in range(0,-100,-1):
+    randomBuilding=ra.randint(1,2)
+    if randomBuilding==1:
+        Entity(model='quad',texture='assets/textures/building1', x=xM)
+    elif randomBuilding==2:
+        Entity(model='quad',texture='assets/textures/building2', x=xM)
 
 app.run()
