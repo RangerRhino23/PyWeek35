ed9afffc1b02c8f23a499cf839ad9e69b4eff113
Itsbailey X <baileyleah542@gmail.com>
2023-03-27 00:04:45 +0100

    cleaned up code allowing for cross-generation

---
+++ GenerateBackground.py
@@ -0,0 +1,35 @@
+prevBuilding=None
+#Gets all the images from the buildings folder
+imageDirectory = 'assets\\textures\\buildings'
+buildingPool = []
+for filename in os.listdir(imageDirectory):
+    if filename.endswith('.png'):
+        buildingPool.append(os.path.join(imageDirectory, filename))
+print(buildingPool)
+#Creates buildings to the right of 0,0
+for x in range(0,100, 1):
+    randomBuilding = ra.choice(buildingPool)
+    while randomBuilding == prevBuilding:
+        randomBuilding = ra.choice(buildingPool)
+    prevBuilding=randomBuilding
+    Entity(model='quad',texture=randomBuilding,x=x,scale=(1,2))
+
+#Creates buildings to the left of 0,0
+for xM in range(-1,-100,-1):
+    randomBuilding = ra.choice(buildingPool)
+    while randomBuilding == prevBuilding:
+        randomBuilding = ra.choice(buildingPool)
+    prevBuilding=randomBuilding
+    Entity(model='quad',texture=randomBuilding,x=xM,scale=(1,2))
+
+for x in np.arange(0.0, 100.0, ra.uniform(1.0, 3.0)):
+    for y in np.arange(1.0, -5.0, ra.uniform(-3.0, -1.0)):
+        if y>-1.005:
+            y=ra.uniform(-1.005,-3)
+        rock = Entity(model='quad', color=rock_color, scale=.1, y=y, x=x, z=ra.uniform(-5.0, -1.0))
+
+for x in np.arange(0.0, -100.0, ra.uniform(-1.0, -3.0)):
+    for y in np.arange(1.0, -5.0, ra.uniform(-3.0, -1.0)):
+        if y>-1.005:
+            y=ra.uniform(-1.005,-3)
+        rock = Entity(model='quad', color=rock_color, scale=.1, y=y, x=x, z=ra.uniform(-5.0, -1.0))
--- Level1.py
+++ Level1.py
@@ -10,7 +10,7 @@ app=Ursina()
 
 CustomColor = rgb
 rock_color = CustomColor(55, 55, 55)
-prevBuilding=None
+
 
 player_controller = PlatformerController2d(scale_y=2, jump_height=2, x=3,model=None, y=20)
 
@@ -21,40 +21,9 @@ camera.add_script(SmoothFollow(target=PlayerAnimation, offset=[0,1,-30], speed=4
 camera.orthographic = True
 camera.fov = 10
 
-#Gets all the images from the buildings folder
-imageDirectory = 'assets\\textures\\buildings'
-buildingPool = []
-for filename in os.listdir(imageDirectory):
-    if filename.endswith('.png'):
-        buildingPool.append(os.path.join(imageDirectory, filename))
-print(buildingPool)
-#Creates buildings to the right of 0,0
-for x in range(0,100, 1):
-    randomBuilding = ra.choice(buildingPool)
-    while randomBuilding == prevBuilding:
-        randomBuilding = ra.choice(buildingPool)
-    prevBuilding=randomBuilding
-    Entity(model='quad',texture=randomBuilding,x=x,scale=(1,2))
 
-#Creates buildings to the left of 0,0
-for xM in range(-1,-100,-1):
-    randomBuilding = ra.choice(buildingPool)
-    while randomBuilding == prevBuilding:
-        randomBuilding = ra.choice(buildingPool)
-    prevBuilding=randomBuilding
-    Entity(model='quad',texture=randomBuilding,x=xM,scale=(1,2))
-
-for x in np.arange(0.0, 100.0, ra.uniform(1.0, 3.0)):
-    for y in np.arange(1.0, -5.0, ra.uniform(-3.0, -1.0)):
-        if y>-1.005:
-            y=ra.uniform(-1.005,-3)
-        rock = Entity(model='quad', color=rock_color, scale=.1, y=y, x=x, z=ra.uniform(-5.0, -1.0))
-
-for x in np.arange(0.0, -100.0, ra.uniform(-1.0, -3.0)):
-    for y in np.arange(1.0, -5.0, ra.uniform(-3.0, -1.0)):
-        if y>-1.005:
-            y=ra.uniform(-1.005,-3)
-        rock = Entity(model='quad', color=rock_color, scale=.1, y=y, x=x, z=ra.uniform(-5.0, -1.0))
+with open("GenerateBackground.py", "r") as f:
+    exec(f.read())
 
 
 sky=Entity(model='quad',texture='assets/textures/sky.jpg',z=100,scale=1000,texture_scale=(30,30))
