c7b1225b34b73126075f95395bf9f87a64beb70c
RangerRhino23 <ryadahlke@outlook.com>
2023-04-01 16:35:20 -0500

    fixed code

--- Tutorial.py
+++ Tutorial.py
@@ -104,7 +104,6 @@ if TutorialCompleted:
             with open("data.json", "w") as f:
                 json.dump(data, f,indent=4)
             invoke(nextpart)
-app.run()
 
 vsyncEnabled=data['vsyncEnabled']
 Fullscreen=data['Fullscreen']
