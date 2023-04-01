bf0cd3fb90d452d92fe4c6eaa2792b923f27f0cd
Itsbailey X <baileyleah542@gmail.com>
2023-04-01 23:24:48 +0100

    updates

--- Finished.py
+++ Finished.py
@@ -5,7 +5,7 @@ app = Ursina()
 # create scrolling text
 scroll_text1 = Text(text='This marks the end of the game.', font='assets/misc/Starjedi.ttf', origin=(0,0), y=-.8, scale=2)
 scroll_text2 = Text(text='This was our pyweek35 game.', font='assets/misc/Starjedi.ttf', origin=(0,0), y=-1, scale=2)
-scroll_text3 = Text(text="And we'd like to thank you for playing", font='assets/misc/Starjedi.ttf', origin=(0,0), y=-1.2, scale=2)
+scroll_text3 = Text(text="And we'd like to thank you for playing.", font='assets/misc/Starjedi.ttf', origin=(0,0), y=-1.2, scale=2)
 scroll_text4 = Text(text='We hope you had an amazing time.', font='assets/misc/Starjedi.ttf', origin=(0,0), y=-1.4, scale=2)
 scroll_text5 = Text(text='Thank you so much! and goodbye.', font='assets/misc/Starjedi.ttf', origin=(0,0), y=-1.6, scale=2)
 
