5f58eb41ad96d80da1400a450c97c66942e06429
Itsbailey X <baileyleah542@gmail.com>
2023-04-01 16:57:17 +0100

    fixed colors and hopefully the vplat jump bug

--- Level3.py
+++ Level3.py
@@ -231,7 +231,7 @@ class MovingPlatform_Vertical(Entity):
 
     def update(self):
         if player_controller.intersects(self) and self.hasCollider:
-            player_controller.y=self.y # modified to update player's y-axis position
+            player_controller.y=self.y+.1 # modified to update player's y-axis position
         self.position += self.direction * self.speed * time.dt
         if self.position.y > self.toY: # modified to check against the top boundary
             self.direction = Vec3(0, -1, 0)
@@ -310,11 +310,11 @@ invisWall1=Entity(model='cube',color=color.clear,y=-8,x=20,scale_y=20,z=player_c
 invisWall1=Entity(model='cube',color=color.clear,y=2,x=27,scale_y=20,z=player_controller.z-.1,scale_z=20,collider='box')
 DoorForWall=Door(locked=True,y=-.5,x=-2)
 blockOne=Entity(ID="Normal",model='quad',color=color.black33,z=player_controller.z,x=6,scale=.3,y=0,collider='box')
-blockTwo=Entity(ID="Inversed",model='quad',color=color.black33,z=player_controller.z,x=7,scale=.3,y=2,collider='box')
+blockTwo=Entity(ID="Inversed",model='quad',color=color.rgb(255,0,255),z=player_controller.z,x=7,scale=.3,y=2,collider='box')
 MovingPlatformOne=MovingPlatform(ID='Normal',color=color.black66,y=2,fromX=8,toX=12)
-blockThree=Entity(ID="Inversed",model='quad',color=color.black33,z=player_controller.z,x=13,scale=.3,y=2,collider='box')
+blockThree=Entity(ID="Inversed",model='quad',color=color.rgb(255,0,255),z=player_controller.z,x=13,scale=.3,y=2,collider='box')
 MovingPlatformTwo=MovingPlatform_Vertical(ID='Normal',color=color.black66,x=14,fromY=3,toY=6)
-blockFour=Entity(ID="Inversed",model='quad',color=color.black33,z=player_controller.z,x=15,scale=.3,y=6,collider='box')
+blockFour=Entity(ID="Inversed",model='quad',color=color.rgb(255,0,255),z=player_controller.z,x=15,scale=.3,y=6,collider='box')
 ground2=Entity(model='quad',color=color.dark_gray,scale_y=.5,z=player_controller.z,scale_x=5,x=18,y=6,collider='box')
 LeverForDoor=Interactable(functionCallBackOn=DoorUnlock,functionCallBackOff=DoorLock,x=20,y=6.5)
 
