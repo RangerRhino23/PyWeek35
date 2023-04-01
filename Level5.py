71b3be8f63aad4dfcfa5aae16d5f094836f734cf
RangerRhino23 <ryadahlke@outlook.com>
2023-04-01 13:24:27 -0500

    fixed inverted colors messed up

--- Level5.py
+++ Level5.py
@@ -73,11 +73,17 @@ def Inverse():
         else:
             if hasattr(e, "ID"):
                 if e.ID=="Normal":
-                    e.color=color.black33
-                    e.collider='box'
+                    if e.name=='LaserBeam':
+                        e.color=color.pink
+                    else:
+                        e.color=color.black33
+                        e.collider='box'
                 if e.ID=="Inversed":
-                    e.color=color.rgb(255,0,255)
-                    e.collider=None
+                    if e.name=='LaserBeam':
+                        e.color=color.red
+                    else:
+                        e.color=color.rgb(255,0,255)
+                        e.collider=None
 
 
 vsyncEnabled=data['vsyncEnabled']
@@ -291,6 +297,7 @@ class LaserBeam(Entity):
         self.color=color.red
         self.cooldown = 0
         self.visible = True
+        self.name = 'LaserBeam'
 
     def update(self):
         self.cooldown = self.cooldown + 1
